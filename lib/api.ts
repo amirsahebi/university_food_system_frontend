c9d69c7fbaa51d2fbb82ec7e2fc8dac64c7c5fe2
Amir <amir_sahebi8080@yahoo.com>
2025-04-06 00:55:46 +0330

    some change and fixes

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://localhost:8000';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://185.110.189.165:8000';
 
 export const API_ROUTES = {
   // Auth
