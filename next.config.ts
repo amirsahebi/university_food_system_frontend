e7338565e1c0b8a85c5e9bbe599e3a082b4649c6
Amir <amir_sahebi8080@yahoo.com>
2025-04-06 21:23:32 +0330

    again fixes for image url

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ['localhost', 'javanfoods.com'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'javanfoods.com',
+        port: '',
+        pathname: '/media/**',
+      }
+    ],
+    unoptimized: false,
   },
 };
 
