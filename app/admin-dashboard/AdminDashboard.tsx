52026e209a8d8495c4778e8b4b8ecd7ef6945d0f
Amir <amir_sahebi8080@yahoo.com>
2025-09-19 12:16:03 +0330

    fix direction of previous and next button in payments managements section in admin dashboard

--- app/admin-dashboard/AdminDashboard.tsx
+++ app/admin-dashboard/AdminDashboard.tsx
@@ -2842,9 +2842,9 @@ export default function AdminDashboard() {
                                   disabled={!pagination.hasPrevious || paymentLoading}
                                   className="px-3"
                                 >
-                                  <span className="ml-1">
+                                  <span className="mr-1">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                                      <path d="m9 18 6-6-6-6"/>
+                                      <path d="m15 18-6-6 6-6"/>
                                     </svg>
                                   </span>
                                   قبلی
@@ -2857,9 +2857,9 @@ export default function AdminDashboard() {
                                   className="px-3"
                                 >
                                   بعدی
-                                  <span className="mr-1">
+                                  <span className="ml-1">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                                      <path d="m15 18-6-6 6-6"/>
+                                      <path d="m9 18 6-6-6-6"/>
                                     </svg>
                                   </span>
                                 </Button>
